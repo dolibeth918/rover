c22d01f610bba870725b24e476854f18e5356ccd
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-10-15 16:14:11 -0400

    api: route to set and get vets for user

--- server/api/users.js
+++ server/api/users.js
@@ -12,3 +12,24 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.get('/:id/vets', async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+    const vetsWithUser = await user.getVets()
+    res.json(vetsWithUser)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/:id/vets', async (req, res, next) => {
+  try {
+    const vet = await Vet.findById(req.body.id)
+    const user = await User.findById(req.params.id)
+    user.addVet(vet)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
