31cdd680795669e441e1a8a774950d4b74e845b2
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-10-15 16:02:23 -0400

    feat: user and vet associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,9 @@
 const User = require('./user')
 const Vet = require('./vet')
+
+User.belongsToMany(Vet, {through: 'user_vet'})
+Vet.belongsToMany(User, {through: 'user_vet'})
+
 module.exports = {
   User,
   Vet
